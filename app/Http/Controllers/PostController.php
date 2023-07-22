ca82e544605fbd971ab142e82cf1068913c94ed4
higuchiyumiko <g001057y@mails.cc.ehime-u.ac.jp>
2023-07-22 14:27:07 +0900

    投稿作成とバリデーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -19,5 +20,13 @@ class PostController extends Controller
     }
     public function show(Post $post){
         return view('posts.show')->with(['post'=>$post]);
+    }
+    public function create(){
+        return view('posts.create');
+    }
+    public function store(Request $request,PostRequest $post){
+        $input=$request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/'.$post->id);
     }
 }
